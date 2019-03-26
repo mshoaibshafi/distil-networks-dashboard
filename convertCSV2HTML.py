0aceda97d5155e351bad88032a2e9217570b573a
mshoaibshafi@yahoo.com <mshoaibshafi@yahoo.com>
2019-03-25 17:11:57 -0700

    Added a work around logic to avoid null values

--- convertCSV2HTML.py
+++ convertCSV2HTML.py
@@ -1,25 +1,32 @@
 # Input CSV File
 # Output HTML Table
 
+import os
 import pandas as pd
 
 def convertCSV2HTML():
-	
-	#TODO : Verify 'data' folder exist ?
+    #TODO : Verify 'data' folder exist ?
 
-	csv_file = 'data/distil.csv'
-	output_distil_html_file = 'data/distil.html'
+    csv_file = 'data/distil.csv'
+    output_distil_html_file = 'data/distil.html'
+    
+    # Delete the html file at the first run or create daily versions
+    if os.path.isfile(output_distil_html_file):
+    	os.remove(output_distil_html_file)
 
 
-	columns = ['Domain Name','domain id','BigIP VIP','machine_learning_threshold','machine_learning_action',\
+
+    # TODO : Create Daily runs of html file ... like distil_html.1 distil.html.2 ... 
+
+    columns = ['Domain Name','domain id','BigIP VIP','machine_learning_threshold','machine_learning_action',\
 				'requests_per_minute','requests_per_minute_action','requests_per_session','requests_per_session_action',\
 				'session_length','session_length_action','known_violators_action','bad_user_agent_action','aggregator_user_agent_action',\
 				'service_provider_action','javascript_action','caching_enabled']
 
 
-	df = pd.read_csv(csv_file, names=columns)
+    df = pd.read_csv(csv_file, names=columns)
 
-	df.to_html(output_distil_html_file)
+    df.to_html(output_distil_html_file)
 
 
 
--- createDashboard.py
+++ createDashboard.py
@@ -18,10 +18,20 @@ def createDashboard(accountID):
 
     # TODO : check if data/domain_info file exist ?
 
-    # TODO : Delete the CSV file at the first run or create daily versions
+    # Delete the CSV file at the first run or create daily versions
+    if os.path.isfile(output_csv_file): 
+        os.remove(output_csv_file)
+
+    # TODO : Create Daily runs of csv file ... like csv_file.1, csv_file.2 ....
+
     # TODO : Create a header for the output_csv_file
 
-
+    # Work Around  - should be removed after checking with Distil Tech Support
+    # Issue  - "web security settings" REST API response has two dict items. 
+    # Sometimes the first item has all the correct values where as the second one is all "null" or vice versa
+    # So work around is 
+    # Check for variable ajax_only for a reference - picked up this one as we are not using this one 
+    # If ajax_only = null use the second item otherwise use the first item
 
     with open(input_file,'r') as f:
         # read the whole csv file into a list
@@ -37,6 +47,13 @@ def createDashboard(accountID):
             # Call extract Web Security 
             w_info = extractWebSecurityInfo(accountID,d[1])
 
+
+            # Work around - explanation give above
+            if w_info['web_security_settings'][0]['ajax_only'] == None:
+                index = 1
+            else:
+                index = 0
+
             # Call XYZ
 
             # Create CSV File
@@ -46,31 +63,31 @@ def createDashboard(accountID):
                 ff.write("{},{},{}".format(d[0],d[1],d[2]))
 
                 # Machine Learning : Threshold and Action
-                ff.write(",{},{}".format(w_info['web_security_settings'][0]['machine_learning_threshold'],\
-                    w_info['web_security_settings'][0]['machine_learning_action']))
+                ff.write(",{},{}".format(w_info['web_security_settings'][index]['machine_learning_threshold'],\
+                    w_info['web_security_settings'][index]['machine_learning_action']))
 
                 # Rate Limiting :
                 # Request per min : Value and Action
-                ff.write(",{},{}".format(w_info['web_security_settings'][0]['requests_per_minute'],\
-                    w_info['web_security_settings'][0]['requests_per_minute_action']))
+                ff.write(",{},{}".format(w_info['web_security_settings'][index]['requests_per_minute'],\
+                    w_info['web_security_settings'][index]['requests_per_minute_action']))
                 # Pages per sesssion : Value and Action
-                ff.write(",{},{}".format(w_info['web_security_settings'][0]['requests_per_session'],\
-                    w_info['web_security_settings'][0]['requests_per_session_action']))
+                ff.write(",{},{}".format(w_info['web_security_settings'][index]['requests_per_session'],\
+                    w_info['web_security_settings'][index]['requests_per_session_action']))
 
                 # Session Lenght : Value and Action
-                ff.write(",{},{}".format(w_info['web_security_settings'][0]['session_length'],\
-                    w_info['web_security_settings'][0]['session_length_action']))
+                ff.write(",{},{}".format(w_info['web_security_settings'][index]['session_length'],\
+                    w_info['web_security_settings'][index]['session_length_action']))
 
                 # Automated Threats Policy : Actions
                 # known Violators, Identities
                 # Aggregator User Agents
                 # Known Violator Data Centers
                 # Automated Browsers
-                ff.write(",{},{}".format(w_info['web_security_settings'][0]['known_violators_action'],\
-                    w_info['web_security_settings'][0]['bad_user_agent_action']))
-                ff.write(",{},{},{}".format(w_info['web_security_settings'][0]['aggregator_user_agent_action'],\
-                    w_info['web_security_settings'][0]['service_provider_action'],\
-                    w_info['web_security_settings'][0]['javascript_action']))
+                ff.write(",{},{}".format(w_info['web_security_settings'][index]['known_violators_action'],\
+                    w_info['web_security_settings'][index]['bad_user_agent_action']))
+                ff.write(",{},{},{}".format(w_info['web_security_settings'][index]['aggregator_user_agent_action'],\
+                    w_info['web_security_settings'][index]['service_provider_action'],\
+                    w_info['web_security_settings'][index]['javascript_action']))
 
                 # Cache Enabled 
                 ff.write(",{}".format(d_info['config_settings']['caching_enabled']))
